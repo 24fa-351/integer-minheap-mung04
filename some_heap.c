934c6d403cd5c41686dbbce3da12f7f87a388b06
Madison Ung <madison.ung@csu.fullerton.edu>
2024-10-03 00:28:18 -0700

    Attempting to fix redefinition errors. Implemented various functions.

--- some_heap.c
+++ some_heap.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "heap.h"
+#include "some_heap.h"
 
 #define KEY_NOT_PRESENT -1
 
@@ -14,13 +14,29 @@ void heap_free(heap_t *heap) {
 
 unsigned int heap_size(heap_t *heap) { return heap->size; }
 
-unsigned int heap_parent(unsigned int index) {  }
+unsigned int heap_parent(unsigned int index) {
+    if (index == 0) {
+        return index;
+    }
+    return (2 * index) / 2;
+}
 
-unsigned int heap_left_child(unsigned int index) { }
+unsigned int heap_left_child(unsigned int index) {
+    return (index * 2) + 1;
+}
 
-unsigned int heap_right_child(unsigned int index) {  }
+unsigned int heap_right_child(unsigned int index) {
+    return (index * 2) + 2;
+}
 
-unsigned int heap_level(unsigned int index) {}
+unsigned int heap_level(unsigned int index) {
+    unsigned int level = 0;
+    while (index > 0) {
+        index = heap_parent(index);
+        level++;
+    }
+    return level;
+}
 
 void heap_print(heap_t *heap) {
     for (int ix = 0; ix < heap_size(heap); ix++) {
@@ -31,12 +47,17 @@ void heap_print(heap_t *heap) {
 }
 
 void heap_swap(heap_t *heap, int index1, int index2) {
+    heap_node_t temp = heap->data[index1];
+    heap->data[index1] = heap->data[index2];
+    heap->data[index2] = temp;
 }
 
 void heap_bubble_up(heap_t *heap, int index) {
+    return;
 }
 
 void heap_bubble_down(heap_t *heap, int index) {
+    return;
 }
 
 void heap_insert(heap_t *heap, heap_key_t key, heap_value_t data) {
@@ -54,7 +75,7 @@ void heap_insert(heap_t *heap, heap_key_t key, heap_value_t data) {
 
 heap_value_t heap_remove_min(heap_t *heap) {
     if (heap_size(heap) == 0) {
-        return NULL;
+        return (heap_value_t){.as_int = KEY_NOT_PRESENT};
     }
 
     heap_value_t min = heap->data[0].value;
